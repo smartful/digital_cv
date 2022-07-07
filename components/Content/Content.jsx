9fcebb865b51b80d7e29065831b7b46b83579906
smartful <remi_rodrigues@hotmail.com>
2022-07-07 23:37:16 +0200

    add buttons content panel

--- components/Content/Content.jsx
+++ components/Content/Content.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Content.module.css";
 
+const content = {
+  background: 1,
+  experiences: 2,
+};
+
 const Content = () => {
-  return <div className={styles.container}>Content</div>;
+  const [display, setDisplay] = useState(content.background);
+
+  const showBackground = () => {};
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.buttonWrapper}>
+        <button
+          onClick={() => setDisplay(content.background)}
+          disabled={display === content.background ? true : false}
+        >
+          Formation
+        </button>
+        <button
+          onClick={() => setDisplay(content.experiences)}
+          disabled={display === content.experiences ? true : false}
+        >
+          Expériences
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Content;
