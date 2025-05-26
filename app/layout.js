c09da4fefc6af19ec071b3c4fec41b6b4c5be765
smartful <remi_rodrigues@hotmail.com>
2025-05-26 02:56:48 +0200

    migration from next 12 to next 15

---
+++ app/layout.js
@@ -0,0 +1,23 @@
+import "../styles/globals.css";
+import styles from "../styles/Home.module.css";
+import Header from "../components/Header/Header";
+
+export const metadata = {
+  title: {
+    default: "Digital CV",
+    template: "%s | Digital CV",
+  },
+  description: "curriculum vitae numérique",
+  icons: { icon: "/favicon.ico" },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body className={styles.container}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
--- components/Content/Content.jsx
+++ components/Content/Content.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useState } from "react";
 import Background from "../Background/Background";
 import Experiences from "../Experiences/Experiences";
